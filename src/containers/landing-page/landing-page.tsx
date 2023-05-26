a4ef4ec8db0948c940a7fd0bf7a0f9f1f9db0281
kevin.rodriguez1 <kevin.rodriguez@edify.cr>
2023-05-26 13:03:25 -0600

    Cleaning files

---
+++ src/containers/landing-page/landing-page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import "./landing-page.styles.css";
+
+const LandingPage: FC = () => {
+  return (
+    <div id="landing-page" className="landing-page">
+      Hello World
+    </div>
+  );
+};
+
+export default LandingPage;
